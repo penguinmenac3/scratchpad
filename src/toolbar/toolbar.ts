e3ca12d7033c7fda5384859a5575825ed2495b40
Michael Fuerst <mail@michaelfuerst.de>
2023-02-24 22:03:39 +0100

    Tools are now selectable with visual indicators.

--- src/toolbar/toolbar.ts
+++ src/toolbar/toolbar.ts
@@ -1,26 +1,26 @@
 import './toolbar.css'
-import { ToolSelector } from './toolselector'
+import { ToolButton } from './toolbutton'
 import { Spacer } from './spacer'
 
 
 export class Toolbar {
   public constructor(
     public mainDiv: HTMLDivElement) {
-    new ToolSelector(mainDiv, "back", '<i class="fa fa-arrow-left" aria-hidden="true"></i>')
-    new ToolSelector(mainDiv, "togglePreview", '<i class="fa fa-columns" aria-hidden="true"></i>')
+    new ToolButton(mainDiv, "back", false, '<i class="fa fa-arrow-left" aria-hidden="true"></i>')
+    new ToolButton(mainDiv, "togglePreview", false, '<i class="fa fa-columns" aria-hidden="true"></i>')
     new Spacer(mainDiv)
-    new ToolSelector(mainDiv, "pen", '<i class="fa fa-circle" aria-hidden="true"></i>')
-    new ToolSelector(mainDiv, "marker", '<i class="fa fa-square" aria-hidden="true"></i>')
-    new ToolSelector(mainDiv, "erasor", '<i class="fa fa-eraser" aria-hidden="true"></i>')
-    new ToolSelector(mainDiv, "text", '<i class="fa fa-font" aria-hidden="true"></i>')
-    new ToolSelector(mainDiv, "tool_settings", '<i class="fa fa-gear" aria-hidden="true"></i>')
+    new ToolButton(mainDiv, "pen", true, '<i class="fa fa-circle" aria-hidden="true"></i>').onClick()
+    new ToolButton(mainDiv, "marker", true, '<i class="fa fa-square" aria-hidden="true"></i>')
+    new ToolButton(mainDiv, "erasor", true, '<i class="fa fa-eraser" aria-hidden="true"></i>')
+    new ToolButton(mainDiv, "text", true, '<i class="fa fa-font" aria-hidden="true"></i>')
+    new ToolButton(mainDiv, "tool_settings", false, '<i class="fa fa-gear" aria-hidden="true"></i>')
     new Spacer(mainDiv)
-    new ToolSelector(mainDiv, "select", '<i class="fa fa-square-o" aria-hidden="true"></i>')
+    new ToolButton(mainDiv, "select", true, '<i class="fa fa-square-o" aria-hidden="true"></i>')
 
-    new ToolSelector(mainDiv, "settings", '<i class="fa fa-bars" aria-hidden="true"></i>', true)
+    new ToolButton(mainDiv, "settings", false, '<i class="fa fa-bars" aria-hidden="true"></i>', true)
     new Spacer(mainDiv, true)
-    new ToolSelector(mainDiv, "redo", '<i class="fa fa-repeat" aria-hidden="true"></i>', true)
-    new ToolSelector(mainDiv, "undo", '<i class="fa fa-undo" aria-hidden="true"></i>', true)
+    new ToolButton(mainDiv, "redo", false, '<i class="fa fa-repeat" aria-hidden="true"></i>', true)
+    new ToolButton(mainDiv, "undo", false, '<i class="fa fa-undo" aria-hidden="true"></i>', true)
     //new Spacer(mainDiv, true)
   }
 }
---
+++ src/toolbar/toolbutton.ts
@@ -0,0 +1,52 @@
+import { Event, Eventbus } from "../eventbus"
+
+export class ToolButton {
+    private static selectableTools: string[] = []
+    protected element: HTMLDivElement
+    
+    public constructor(protected toolbar: HTMLDivElement,  protected id: string, selectable: boolean, innerHTML: string = "", right: boolean = false)
+    {
+        this.element = document.createElement("div")
+        toolbar.appendChild(this.element)
+        this.element.classList.add("tool")
+        if (right) {
+            this.element.style.float = "right"
+        }
+        if (selectable) {
+            ToolButton.selectableTools.push(this.id)
+        }
+        if (innerHTML == "") {
+            innerHTML = `id_${id}`
+        }
+        this.setContent(innerHTML)
+        this.element.onclick = this.onClick.bind(this)
+        Eventbus.register("toolbar/change", this.onSelectionChanged.bind(this))
+    }
+
+    public onClick(_: MouseEvent | null = null) {
+        Eventbus.send("toolbar/change", {
+            "type": "string", "data": this.id, "allowNetwork": false
+        })
+        return true
+    }
+
+    protected onSelectionChanged(topic: string, event: Event): void {
+        if (topic == "toolbar/change" && event.type == "string"){
+            if (ToolButton.selectableTools.indexOf(event.data) == -1) return
+            if (event.data == this.id) {
+                if (!this.element.classList.contains("selected-tool")){
+                    this.element.classList.add("selected-tool")
+                }
+            } else {
+                if (this.element.classList.contains("selected-tool")){
+                    this.element.classList.remove("selected-tool")
+                }
+            }
+        }
+        return
+    }
+
+    protected setContent(html: string) {
+        this.element.innerHTML = html
+    }
+}
