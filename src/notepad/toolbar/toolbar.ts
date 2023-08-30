93ad750e1b8631c1c8898b63d892b22e2303a916
Michael Fuerst <Michael.Fuerst@f-online.net>
2023-08-30 22:51:36 +0200

    Update all toolbar icons.

--- src/notepad/toolbar/toolbar.ts
+++ src/notepad/toolbar/toolbar.ts
@@ -2,40 +2,42 @@ import './toolbar.css'
 import { ToolButton, ToolPopup } from './toolbutton'
 import { Spacer } from './spacer'
 import { Module } from '../../webui/module'
+import { iconArrowLeft, iconErasor, iconMarker, iconPen, iconRectangle, iconRedo, iconSave, iconSelect, iconSplit, iconText, iconUndo } from './icons'
+import { iconBars } from '../../webui/icons/icons'
 
 
 export class Toolbar extends Module<HTMLDivElement> {
   public constructor() {
     super("div", "", "toolbar")
-    this.add(new ToolButton("back", false, '<i class="fa fa-arrow-left" aria-hidden="true"></i>'))
-    this.add(new ToolButton("togglePreview", false, '<i class="fa fa-columns" aria-hidden="true"></i>'))
-    this.add(new ToolButton("save", false, '<i class="fa fa-gear" aria-hidden="true"></i>'))
+    this.add(new ToolButton("back", false, iconArrowLeft))
+    this.add(new ToolButton("togglePreview", false, iconSplit))
+    this.add(new ToolButton("save", false, iconSave))
     this.add(new Spacer())
-    let pen = new ToolButton("tool_pen", true, '<i class="fa fa-circle" aria-hidden="true"></i>')
+    let pen = new ToolButton("tool_pen", true, iconPen)
     pen.onClick()
     pen.addPopup(new ToolPopup())
     this.add(pen)
-    let marker = new ToolButton("tool_marker", true, '<i class="fa fa-square" aria-hidden="true"></i>')
+    let marker = new ToolButton("tool_marker", true, iconMarker)
     marker.addPopup(new ToolPopup())
     this.add(marker)
-    let erasor = new ToolButton("tool_erasor", true, '<i class="fa fa-eraser" aria-hidden="true"></i>')
+    let erasor = new ToolButton("tool_erasor", true, iconErasor)
     erasor.addPopup(new ToolPopup())
     this.add(erasor)
-    let text = new ToolButton("tool_text", true, '<i class="fa fa-font" aria-hidden="true"></i>')
+    let text = new ToolButton("tool_text", true, iconText)
     text.addPopup(new ToolPopup())
     this.add(text)
     this.add(new Spacer())
-    let shape = new ToolButton("tool_shape", true, '<i class="fa fa-square-o" aria-hidden="true"></i>')
+    let shape = new ToolButton("tool_shape", true, iconRectangle)
     shape.addPopup(new ToolPopup())
     this.add(shape)
-    let select = new ToolButton("tool_select", true, '<i class="fa fa-square-o" aria-hidden="true"></i>') // TODO LASSO
+    let select = new ToolButton("tool_select", true, iconSelect)
     select.addPopup(new ToolPopup())
     this.add(select)
 
-    this.add(new ToolButton("settings_document", false, '<i class="fa fa-bars" aria-hidden="true"></i>', true))
+    this.add(new ToolButton("settings_document", false, iconBars, true))
     this.add(new Spacer(true))
-    this.add(new ToolButton("redo", false, '<i class="fa fa-repeat" aria-hidden="true"></i>', true))
-    this.add(new ToolButton("undo", false, '<i class="fa fa-undo" aria-hidden="true"></i>', true))
+    this.add(new ToolButton("redo", false, iconRedo, true))
+    this.add(new ToolButton("undo", false, iconUndo, true))
     //this.add(new Spacer(true))
   }
 }
